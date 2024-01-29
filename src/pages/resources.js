0e257e99a137b2b6ba3d9c001b77da2ada7588c5
David Fekke <david@fekke.com>
2024-01-29 13:08:37 -0500

    Modernized the React component for Resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 import Layout from "../components/layout"
 import Navbar from "../components/navbar"
@@ -12,50 +12,30 @@ import { ToastContainer, toast } from 'react-toastify';
 import { FaFacebook, FaLinkedin, FaInstagram, FaGalacticRepublic } from 'react-icons/fa'; 
 import 'react-toastify/dist/ReactToastify.css';
 
-class Resources extends React.Component {
-    constructor() {
-        super();
-        
-        this.state = { 
-            currentweather: '',
-            altim: '30.03',
-            wind_dir: 0,
-            wind_speed: 0,
-            gust: 0,
-            visibility: 0,
-            temp: 0,
-            dew: 0,
-            avwxcolor: 'black',
-            sky_condition: [],
-            raw_text: '',
-            forecast: [],
-            forecast_raw_text: ''
-        };
+const ResourcesComponent = () => {
+    const defaultWeather = {
+        currentweather: '',
+        altim: '30.03',
+        wind_dir: 0,
+        wind_speed: 0,
+        gust: 0,
+        visibility: 0,
+        temp: 0,
+        dew: 0,
+        avwxcolor: 'black',
+        sky_condition: [],
+        raw_text: '',
+        forecast: [],
+        forecast_raw_text: ''
+    };
 
-        this.buttonRefresh = this.buttonRefresh.bind(this);
-        this.refresh = this.refresh.bind(this);
+    const [weather, setWeather] = useState(defaultWeather);
 
-        toast.configure();
-        this.notify = this.notify.bind(this);
-    }
-    
-    componentDidMount() {
-        //https://avwxproxy.herokuapp.com/metar/kcrg
-        this.refresh();
-    }
-
-    notify() {
+    const notify = () => {
         toast.success('Weather refreshed!');
     } 
-
-    buttonRefresh(event) {
-        console.log('Weather refreshed');
-                
-        event.preventDefault();
-        this.refresh();
-    }
-
-    refresh() {
+    
+    const refresh = () => {
         axios.get('https://avwx.fekke.com/metar/kcrg').then(results => {
             let avwxcolor = 'black';
             //console.log(results.data[0]);
@@ -82,7 +62,7 @@ class Resources extends React.Component {
             } else {
                 current_sky_condition.push(current_weather.sky_condition);
             }
-            this.setState({ currentweather: results.data[0].flight_category, 
+            setWeather({ currentweather: results.data[0].flight_category, 
                 altim: parseFloat(results.data[0].altim_in_hg).toFixed(2), 
                 wind_dir: results.data[0].wind_dir_degrees,
                 wind_speed: results.data[0].wind_speed_kt,
@@ -94,143 +74,145 @@ class Resources extends React.Component {
                 avwxcolor,
                 raw_text: results.data[0].raw_text
             });
-            this.notify();
+            notify();
         });
-
+    
         axios.get('https://avwx.fekke.com/taf/kcrg').then(results => {   
-            this.setState({ 
+            setWeather({ 
                 forecast: results.data[0].forecast,
                 forecast_raw_text: results.data[0].raw_text
             });
         });
     }
-    
-    render() {
-        return (<Layout>
-            <Helmet title="Resources" />
-            <ToastContainer autoClose={2000} />
-            <Navbar />
-            <Header headline="Resources" />
-            <Article>
-                <h1>Resources</h1>
 
-                <p>Here are some great resources available to pilots.</p>
+    useEffect(() => {
+        refresh();
+    }, []);
+
+    return (<Layout>
+        <Helmet title="Resources" />
+        <ToastContainer autoClose={2000} />
+        <Navbar />
+        <Header headline="Resources" />
+        <Article>
+            <h1>Resources</h1>
+
+            <p>Here are some great resources available to pilots.</p>
+            
+            <div style={{ display: 'grid', gridGap: '10px', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))'}}>
                 
-                <div style={{ display: 'grid', gridGap: '10px', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))'}}>
-                    
-                    <div className="resourceBlock" style={{ gridRow: '1 / 3'}}>
-                        <h3>Current Weather</h3>
-                        <button style={{ fontSize: '1rem', 
-                                                borderRadius: '3px', 
-                                                backgroundColor: 'lightblue',
-                                                padding: '0.5rem',
-                                                marginTop: '1rem' }} 
-                                onClick={this.refresh}>Refresh</button><br /><br />
-                        <strong>Current Weather at KCRG:</strong> <span style={{ color: `${this.state.avwxcolor}`, fontWeight: 'bold'}}> {this.state.currentweather}</span><br />
-                        <strong>Sky Condition: </strong>
-                            <ul>{this.state.sky_condition && this.state.sky_condition.map(cond => {
-                                    return (<li key={cond.cloud_base_ft_agl}>{cond.sky_cover} {cond.cloud_base_ft_agl}</li>) 
-                                })}
+                <div className="resourceBlock" style={{ gridRow: '1 / 3'}}>
+                    <h3>Current Weather</h3>
+                    <button style={{ fontSize: '1rem', 
+                                            borderRadius: '3px', 
+                                            backgroundColor: 'lightblue',
+                                            padding: '0.5rem',
+                                            marginTop: '1rem' }} 
+                            onClick={refresh}>Refresh</button><br /><br />
+                    <strong>Current Weather at KCRG:</strong> <span style={{ color: `${weather.avwxcolor}`, fontWeight: 'bold'}}> {weather.currentweather}</span><br />
+                    <strong>Sky Condition: </strong>
+                        <ul>{weather.sky_condition && weather.sky_condition.map(cond => {
+                                return (<li key={cond.cloud_base_ft_agl}>{cond.sky_cover} {cond.cloud_base_ft_agl}</li>) 
+                            })}
+                        </ul>
+                    <strong>Altimeter:</strong> {weather.altim}<br />
+                    <strong>Visibility:</strong> {weather.visibility}<br />
+                    <strong>Wind Direction:</strong> {weather.wind_dir}<br />
+                    <strong>Wind Speed:</strong> {weather.wind_speed}<br />
+                    <strong>Wind Gust:</strong> {weather.gust}<br />
+                    <strong>Temperature:</strong> {weather.temp} (Celcius)<br />
+                    <strong>Dewpoint:</strong> {weather.dew} (Celcius)<br />
+                    <strong>Raw Text:</strong> {weather.raw_text}<br /><br />
+                        
+
+                    <strong>Forcasted weather at KCRG:</strong><br /><br />
+                    {weather.forecast.map(item => {
+                        const from_date = moment(item.fcst_time_from).format('YYYY-MM-DD h:mm a');
+                        const to_date = moment(item.fcst_time_to).format('YYYY-MM-DD h:mm a');
+                        return (<div key={item.fcst_time_from}>
+                            <strong>From:</strong> {from_date}&nbsp;
+                            <strong>to:</strong> {to_date} <br />
+                            <strong>Sky Conditions: </strong>
+                            <ul>
+                                {(item.sky_condition && item.sky_condition.map(cond => <li key={cond.cloud_base_ft_agl}>{cond.sky_cover} {cond.cloud_base_ft_agl}</li>))}
                             </ul>
-                        <strong>Altimeter:</strong> {this.state.altim}<br />
-                        <strong>Visibility:</strong> {this.state.visibility}<br />
-                        <strong>Wind Direction:</strong> {this.state.wind_dir}<br />
-                        <strong>Wind Speed:</strong> {this.state.wind_speed}<br />
-                        <strong>Wind Gust:</strong> {this.state.gust}<br />
-                        <strong>Temperature:</strong> {this.state.temp} (Celcius)<br />
-                        <strong>Dewpoint:</strong> {this.state.dew} (Celcius)<br />
-                        <strong>Raw Text:</strong> {this.state.raw_text}<br /><br />
-                            
+                            <strong>Wind direction:</strong> {item.wind_dir_degrees}<br />
+                            <strong>Wind speed:</strong> {item.wind_speed_kt}<br />
+                            {item.wind_gust_kt && <><strong>Wind gust:</strong> {item.wind_gust_kt}<br /></> }
+                            <strong>Visibility:</strong> {parseInt(item.visibility_statute_mi)}<br /> 
+                            <br />
+                        </div>)
+                    })}
+                    Raw Text: {this.state.forecast_raw_text}<br /><br />
+                </div>
 
-                        <strong>Forcasted weather at KCRG:</strong><br /><br />
-                        {this.state.forecast.map(item => {
-                            const from_date = moment(item.fcst_time_from).format('YYYY-MM-DD h:mm a');
-                            const to_date = moment(item.fcst_time_to).format('YYYY-MM-DD h:mm a');
-                            return (<div key={item.fcst_time_from}>
-                                <strong>From:</strong> {from_date}&nbsp;
-                                <strong>to:</strong> {to_date} <br />
-                                <strong>Sky Conditions: </strong>
-                                <ul>
-                                    {(item.sky_condition && item.sky_condition.map(cond => <li key={cond.cloud_base_ft_agl}>{cond.sky_cover} {cond.cloud_base_ft_agl}</li>))}
-                                </ul>
-                                <strong>Wind direction:</strong> {item.wind_dir_degrees}<br />
-                                <strong>Wind speed:</strong> {item.wind_speed_kt}<br />
-                                {item.wind_gust_kt && <><strong>Wind gust:</strong> {item.wind_gust_kt}<br /></> }
-                                <strong>Visibility:</strong> {parseInt(item.visibility_statute_mi)}<br /> 
-                                <br />
-                            </div>)
-                        })}
-                        Raw Text: {this.state.forecast_raw_text}<br /><br />
-                    </div>
-
-                    <div className="resourceBlock" >
-                        <h3>Weather</h3>
-                        <p>
-                            <ExtLink uri="https://www.1800wxbrief.com" name="Leidos 1-800-WX-BRIEF" /><br />
-                            <ExtLink uri="https://www.aviationweather.gov/" name="Aviation Weather" /><br />
-                            <ExtLink uri="http://www.usairnet.com/cgi-bin/launch/code.cgi?Submit=Go&sta=KCRG&state=FL" name="US Airnet" /><br />
-                        </p>
-                    </div>
-                    
-                    <div className="resourceBlock" >
-                        <h3>Flight Planning</h3>
-                            
-                        <p>
-                            <ExtLink uri="https://www.foreflight.com" name="ForeFlight" /><br />
-                            <ExtLink uri="https://www.fltplan.com" name="Flight Plan" /><br />
-                            <ExtLink uri="https://www.skyvector.com" name="Sky Vector" /><br />
-                            <ExtLink uri="https://www.windy.com" name="Windy" /><br />
-                        </p>
-                    </div>
-                    
-                    <div className="resourceBlock">
-                        <h3>Parts and Gear</h3>
-                        <p>
-                            <ExtLink uri="https://www.sportys.com/pilotshop" name="Sporty's Pilot Shop" /><br />
-                            <ExtLink uri="https://www.pilotmall.com/" name="Pilot Mall" /><br />
-                            <ExtLink uri="https://www.aircraftgeneralsupply.com" name="Aircraft General Supply" /><br />
-                            <ExtLink uri="https://www.aircraftspruce.com" name="Aircraft Spruce" /><br />
-                        </p>
-                    </div>
-                    
-                    <div className="resourceBlock" >
-                        <h3>Social Media</h3>
-                        <p>
-                        <a href = "https://www.facebook.com/groups/849967972062524/"
-                            target = "_blank"
-                            rel = "noopener noreferrer"
-                            style = {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center' }}> 
-                            <FaFacebook size = { 20 } style = {{ padding: '5px', color: 'black' }} /> Facebook
-                        </a> 
-                        <a href="https://www.linkedin.com/groups/12242408/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            style= {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center'}}>
-                            <FaLinkedin size={ 20 } style={{ padding: '5px', color: 'black' }} /> Linkedin
-                        </a>
-                        <a href="https://www.instagram.com/capa_flies/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            style= {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center' }}>
-                            <FaInstagram size={ 20 } style={{ padding: '5px', color: 'black' }} /> Instagram
-                        </a>
-                        <a href="craigpilots.locals.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            style= {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center' }}>
-                                <FaGalacticRepublic size={ 20 } style={{ padding: '5px', color: 'black' }} /> Craig Pilots Locals
-                            </a>
-                        </p>
-                    </div>
-                    
-                    
-
+                <div className="resourceBlock" >
+                    <h3>Weather</h3>
+                    <p>
+                        <ExtLink uri="https://www.1800wxbrief.com" name="Leidos 1-800-WX-BRIEF" /><br />
+                        <ExtLink uri="https://www.aviationweather.gov/" name="Aviation Weather" /><br />
+                        <ExtLink uri="http://www.usairnet.com/cgi-bin/launch/code.cgi?Submit=Go&sta=KCRG&state=FL" name="US Airnet" /><br />
+                    </p>
                 </div>
                 
-            </Article>
-            <Footer />
-        </Layout>)
-    }
+                <div className="resourceBlock" >
+                    <h3>Flight Planning</h3>
+                        
+                    <p>
+                        <ExtLink uri="https://www.foreflight.com" name="ForeFlight" /><br />
+                        <ExtLink uri="https://www.fltplan.com" name="Flight Plan" /><br />
+                        <ExtLink uri="https://www.skyvector.com" name="Sky Vector" /><br />
+                        <ExtLink uri="https://www.windy.com" name="Windy" /><br />
+                    </p>
+                </div>
+                
+                <div className="resourceBlock">
+                    <h3>Parts and Gear</h3>
+                    <p>
+                        <ExtLink uri="https://www.sportys.com/pilotshop" name="Sporty's Pilot Shop" /><br />
+                        <ExtLink uri="https://www.pilotmall.com/" name="Pilot Mall" /><br />
+                        <ExtLink uri="https://www.aircraftgeneralsupply.com" name="Aircraft General Supply" /><br />
+                        <ExtLink uri="https://www.aircraftspruce.com" name="Aircraft Spruce" /><br />
+                    </p>
+                </div>
+                
+                <div className="resourceBlock" >
+                    <h3>Social Media</h3>
+                    <p>
+                    <a href = "https://www.facebook.com/groups/849967972062524/"
+                        target = "_blank"
+                        rel = "noopener noreferrer"
+                        style = {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center' }}> 
+                        <FaFacebook size = { 20 } style = {{ padding: '5px', color: 'black' }} /> Facebook
+                    </a> 
+                    <a href="https://www.linkedin.com/groups/12242408/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style= {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center'}}>
+                        <FaLinkedin size={ 20 } style={{ padding: '5px', color: 'black' }} /> Linkedin
+                    </a>
+                    <a href="https://www.instagram.com/capa_flies/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style= {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center' }}>
+                        <FaInstagram size={ 20 } style={{ padding: '5px', color: 'black' }} /> Instagram
+                    </a>
+                    <a href="craigpilots.locals.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style= {{ textDecoration: 'none', color: 'black', display: 'flex', alignItems: 'center' }}>
+                            <FaGalacticRepublic size={ 20 } style={{ padding: '5px', color: 'black' }} /> Craig Pilots Locals
+                        </a>
+                    </p>
+                </div>
+                
+                
+
+            </div>
+            
+        </Article>
+        <Footer />
+    </Layout>);   
 }
 
-export default Resources;
+export default ResourcesComponent;
